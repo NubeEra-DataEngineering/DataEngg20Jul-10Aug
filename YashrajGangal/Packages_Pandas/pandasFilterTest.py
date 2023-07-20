fcc082789029cab5c2c61698d22de916acf8cb4e
yashrajg <gangalyashraj@gmail.com>
2023-07-20 15:48:40 -0400

    Pandas Filter Program added

---
+++ YashrajGangal/Packages_Pandas/pandasFilterTest.py
@@ -0,0 +1,9 @@
+import pandas
+
+dfEmployees = pandas.read_csv("data.csv")
+
+dfEmployees = dfEmployees[dfEmployees["Age"] > 100]
+
+dfEmployees = dfEmployees[["Name", "Age", "City"]]
+
+dfEmployees.to_csv("filteredData.csv")
