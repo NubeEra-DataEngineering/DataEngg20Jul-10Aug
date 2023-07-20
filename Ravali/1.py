02b4dd1a6c2a0e27243ebceeb5a24ecd150c22f3
Ravali Satla <ravali.satla@incedoinc.com>
2023-07-20 13:35:30 -0400

    updated 2 python questions

---
+++ Ravali/1.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self, emp_id, emp_name, emp_salary):
+        self.emp_id = emp_id
+        self.emp_salary = emp_salary
+
+    def employee_info(self):
+        print(f"Employee ID: {self.emp_id}")
+        print(f"Employee Salary: ${self.emp_salary:.2f}")
+
+
+employee1 = Employee(101, "John Doe", 50000.50)
+employee1.employee_info()
+
+
+
+
+
+
