e426e96bef183258a921c4186ff8492b73ea1e69
tan-incedo <taniya.chauhan1@incedoinc.com>
2023-07-20 13:17:14 -0700

    changes

--- TaniyaChauhan/Visualization/line_plot.py
+++ TaniyaChauhan/Visualization/line_plot.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
 sales = [10000, 12000, 9000, 15000, 18000, 14000]
 plt.plot(months, sales)
+plt.xlabel('Months')
+plt.ylabel('Sales')
 plt.show()
