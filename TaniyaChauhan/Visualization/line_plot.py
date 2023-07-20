501236c0610ed575b8b347d34400497c0a595813
tan-incedo <taniya.chauhan1@incedoinc.com>
2023-07-20 13:12:36 -0700

    line plot proceed

---
+++ TaniyaChauhan/Visualization/line_plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+# Data
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+sales = [10000, 12000, 9000, 15000, 18000, 14000]
+plt.plot(months, sales)
+plt.show()
