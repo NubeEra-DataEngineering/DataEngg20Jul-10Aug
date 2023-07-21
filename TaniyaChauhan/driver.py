c74afadcee19d392f38fabd17be3d581247a31dd
tan-incedo <taniya.chauhan1@incedoinc.com>
2023-07-21 13:46:33 -0700

    creation+upload

--- TaniyaChauhan/driver.py
+++ TaniyaChauhan/driver.py
@@ -1,5 +1,14 @@
 import boto3
 
+AWS_REGION = "ap-south-1"
+resource = boto3.resource("s3", region_name=AWS_REGION)
+bucket_name = "bkt-taniya-assignment"
+location = {'LocationConstraint': AWS_REGION}
+bucket = resource.create_bucket(
+    Bucket=bucket_name,
+    CreateBucketConfiguration=location)
+print("Amazon S3 bucket has been created")
+
 def upload_file(file_name, bucket, object_name=None, args=None):
     if object_name is None:
         object_name = file_name
