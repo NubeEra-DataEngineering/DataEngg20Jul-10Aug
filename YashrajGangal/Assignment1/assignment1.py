4b7273935c07068074692a75a0d96867b17ba63d
yashrajg <gangalyashraj@gmail.com>
2023-07-26 12:47:00 -0400

    Separated Helper Code in S3 event publishing

--- YashrajGangal/Assignment1/assignment1.py
+++ YashrajGangal/Assignment1/assignment1.py
@@ -1,30 +1,47 @@
 import boto3
 import json
 
-def lambda_handler(event, context):
+class AWSHandler:
+
+    def __init__(self, SNSTopicARN) -> None:
+        self.SNSTopicARN = SNSTopicARN
+        self.SNSClient = boto3.client('sns')
+
+    @staticmethod
+    def getBucketEventDetails(event) -> tuple:
+        bucketName = event['Records'][0]['s3']['bucket']['name']
+        fileName = event['Records'][0]['s3']['object']['key']
+        action = event['Records'][0]['eventName']
+
+        return bucketName, fileName, action
     
-    # Read bucket event details
-    # print(event)
-    bucketName = event['Records'][0]['s3']['bucket']['name']
-    fileName = event['Records'][0]['s3']['object']['key']
-    action = event['Records'][0]['eventName']
-    message = f'Action {action} performed on file {fileName} in bucket {bucketName}'
-    
-    # publish to SNS topic
-    sns = boto3.client('sns')
-    try:
-        # Publish a message to the the Demo_Topic
-        topic_arn = 'arn:aws:sns:ap-south-1:475184346033:yashraj-assignment1-bucketActionReport'
-        
-        response = sns.publish(
-            TopicArn=topic_arn,
+    def publishMessage(self, message: str):
+        response = self.SNSClient.publish(
+            TopicArn=self.SNSTopicARN,
             Message= message
         )
         
         print(f'Message "{message}" published to SNS topic')
+
+        return response
+
+
+def lambda_handler(event, context):
+    
+    # Read bucket event details
+    topic_arn = 'arn:aws:sns:ap-south-1:475184346033:yashraj-assignment1-bucketActionReport'
+
+    helper = AWSHandler(topic_arn)
+
+    bucketName,  fileName, action = AWSHandler.getBucketEventDetails(event=event)
+    message = f'Action {action} performed on file {fileName} in bucket {bucketName}'
+    
+    # publish to SNS topic
+    try:
+        SNSClientResponse = helper.publishMessage(message)
         return {
             'statusCode': 200,
-            'body': json.dumps(response)
+            'body': json.dumps(SNSClientResponse)
         }
     except Exception as e:
         print('Failed to publish message to SNS topic')
