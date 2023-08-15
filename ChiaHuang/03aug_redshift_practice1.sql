0e16f4c76ef89601423b2f122a53dbd94bd1ce8c
chiachunhuang <140101002+chiachunhuang@users.noreply.github.com>
2023-08-15 12:38:46 -0400

    Create 03aug_redshift_practice1.sql

---
+++ ChiaHuang/03aug_redshift_practice1.sql
@@ -0,0 +1,140 @@
+CREATE TABLE supplier_chiah 
+(
+  s_suppkey   INTEGER NOT NULL,
+  s_name      VARCHAR(25) NOT NULL,
+  s_address   VARCHAR(25) NOT NULL,
+  s_city      VARCHAR(10) NOT NULL,
+  s_nation    VARCHAR(15) NOT NULL,
+  s_region    VARCHAR(12) NOT NULL,
+  s_phone     VARCHAR(15) NOT NULL
+);
+
+COPY dev.public.supplier_chiah FROM 's3://awssampledbuswest2/ssbgz/supplier.tbl'
+CREDENTIALS 'aws_iam_role=arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+DELIMITER '|'
+gzip
+REGION 'us-west-2';
+
+select * from supplier_chiah;
+
+CREATE TABLE customer_chiah
+(
+  c_custkey      INTEGER NOT NULL, 
+  c_name         VARCHAR(25) NOT NULL,
+  c_address      VARCHAR(25) NOT NULL,
+  c_city         VARCHAR(10) NOT NULL,
+  c_nation       VARCHAR(15) NOT NULL,
+  c_region       VARCHAR(12) NOT NULL,
+  c_phone        VARCHAR(15) NOT NULL,
+  c_mktsegment   VARCHAR(10) NOT NULL
+);
+
+COPY dev.public.customer_chiah FROM 's3://bkt-03aug-mujahed/load/customer-fw-manifest'
+CREDENTIALS 'aws_iam_role=arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+fixedwidth 'c_custkey:10,c_name:25,c_address:25,c_city:10,c_nation:15,c_region:12,c_phone:15,c_mktsegment:10'
+maxerror 10
+acceptinvchars as '^'
+manifest;
+
+select COUNT(*) from dev.public.supplier_chiah
+
+CREATE TABLE dev.public.dwdate_chiah
+(
+  d_datekey            INTEGER NOT NULL,
+  d_date               VARCHAR(19) NOT NULL,
+  d_dayofweek          VARCHAR(10) NOT NULL,
+  d_month              VARCHAR(10) NOT NULL,
+  d_year               INTEGER NOT NULL,
+  d_yearmonthnum       INTEGER NOT NULL,
+  d_yearmonth          VARCHAR(8) NOT NULL,
+  d_daynuminweek       INTEGER NOT NULL,
+  d_daynuminmonth      INTEGER NOT NULL,
+  d_daynuminyear       INTEGER NOT NULL,
+  d_monthnuminyear     INTEGER NOT NULL,
+  d_weeknuminyear      INTEGER NOT NULL,
+  d_sellingseason      VARCHAR(13) NOT NULL,
+  d_lastdayinweekfl    VARCHAR(1) NOT NULL,
+  d_lastdayinmonthfl   VARCHAR(1) NOT NULL,
+  d_holidayfl          VARCHAR(1) NOT NULL,
+  d_weekdayfl          VARCHAR(1) NOT NULL
+);
+
+COPY dev.public.dwdate_chiah FROM 's3://bkt-03aug-mujahed/load/dwdate-tab.tbl'
+CREDENTIALS 'aws_iam_role=arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608' 
+DELIMITER '\t'
+dateformat 'auto';
+
+select COUNT(*) from dev.public.dwdate_chiah;
+
+CREATE TABLE lineorder_chiah
+(
+  lo_orderkey          INTEGER NOT NULL,
+  lo_linenumber        INTEGER NOT NULL,
+  lo_custkey           INTEGER NOT NULL,
+  lo_partkey           INTEGER NOT NULL,
+  lo_suppkey           INTEGER NOT NULL,
+  lo_orderdate         INTEGER NOT NULL,
+  lo_orderpriority     VARCHAR(15) NOT NULL,
+  lo_shippriority      VARCHAR(1) NOT NULL,
+  lo_quantity          INTEGER NOT NULL,
+  lo_extendedprice     INTEGER NOT NULL,
+  lo_ordertotalprice   INTEGER NOT NULL,
+  lo_discount          INTEGER NOT NULL,
+  lo_revenue           INTEGER NOT NULL,
+  lo_supplycost        INTEGER NOT NULL,
+  lo_tax               INTEGER NOT NULL,
+  lo_commitdate        INTEGER NOT NULL,
+  lo_shipmode          VARCHAR(10) NOT NULL
+);
+
+COPY  lineorder_chiah
+FROM 's3://awssampledb/load/lo/lineorder-single.tbl' 
+CREDENTIALS 'aws_iam_role=arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608' 
+GZIP
+COMPUPDATE OFF
+REGION 'us-east-1';
+
+select COUNT(*) from dev.public.lineorder_chiah;
+
+Unload ('select * from customer_chiah')
+TO 's3://bkt-03aug-chiah/unload/customer_chiah'
+IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608';
+
+Unload ('select * from dwdate_chiah')
+TO 's3://bkt-03aug-chiah/unload/dwdate_chiah'
+IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+Parallel OFF
+
+Unload ('select * from lineorder_chiah')
+TO 's3://bkt-03aug-chiah/unload/lineorder_chiah'
+IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+Parallel OFF
+MAXFILESIZE 100 MB;
+
+Unload ('select * from supplier_chiah')
+TO 's3://bkt-03aug-chiah/unload/supplier_chiah'
+IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+Manifest; -- lists the names and locations of data files stored in a distributed storage system
+
+
+CREATE TABLE region_nations_noshred_chiah (rdata SUPER);
+
+COPY region_nations_noshred_chiah FROM 's3://redshift-downloads/semistructured/tpch-nested/data/json/region_nation'
+REGION 'us-east-1' IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+FORMAT JSON 'noshred';
+
+select * from region_nations_noshred_chiah
+
+CREATE TABLE region_nations_chiah
+(
+ r_regionkey smallint
+ ,r_name varchar
+ ,r_comment varchar
+ ,r_nations super
+ );
+
+COPY region_nations_chiah FROM 's3://redshift-downloads/semistructured/tpch-nested/data/json/region_nation'
+REGION 'us-east-1' IAM_ROLE 'arn:aws:iam::475184346033:role/service-role/AmazonRedshift-CommandsAccessRole-20230802T180608'
+FORMAT JSON 'auto';
+
+select * from region_nations_chiah;
